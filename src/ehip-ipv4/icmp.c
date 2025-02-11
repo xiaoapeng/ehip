5827cced1344e715a4cdcc307a54f0168a0ef48e
simon.xiaoapeng <simon.xiaoapeng@gmail.com>
2025-02-11 21:30:24 +0800

    feat(ehip-ipv4): 实现 ICMP 输入处理
    - 添加 ICMP 输入处理函数，解析 ICMP 报文头部
    - 实现 ICMP 回显请求和回显应答的处理
    - 优化代码结构，提高可读性和可维护性

--- src/ehip-ipv4/icmp.c
+++ src/ehip-ipv4/icmp.c
@@ -12,8 +12,27 @@
 
 #include <ehip-ipv4/icmp.h>
 #include <ehip-ipv4/ip_message.h>
+
 int icmp_input(struct ip_message *ip_msg){
-    (void)ip_msg;
+    int ret;
+    struct icmp_hdr *icmp_hdr;
+    struct icmp_hdr icmp_hdr_tmp;
+
+    ret = ip_message_rx_read(ip_msg, (uint8_t**)&icmp_hdr, sizeof(struct icmp_hdr), (uint8_t*)&icmp_hdr_tmp);
+    if(ret != sizeof(struct icmp_hdr)){
+        goto drop;
+    }
+    switch(icmp_hdr->type){
+        case ICMP_TYPE_ECHO_REPLY:
+        case ICMP_TYPE_ECHO:{
+            void ping_input(struct ip_message *ip_msg, const struct icmp_hdr *icmp_hdr);
+            ping_input(ip_msg, icmp_hdr);
+            break;
+        }
+        default:
+            break;
+    }
+drop:
     ip_message_free(ip_msg);
     return 0;
 }
