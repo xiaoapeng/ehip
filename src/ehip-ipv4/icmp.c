0e536eb8fa9e27df48145712cc22168336557e77
simon.xiaoapeng <simon.xiaoapeng@gmail.com>
2025-04-19 16:47:51 +0800

    fix(icmp): 修复 ICMP 校验和计算错误
    - 添加了 ICMP 校验和的计算和验证逻辑
    - 处理了分片数据的校验和计算
    - 如果校验和错误，会丢弃数据包并记录警告日志

--- src/ehip-ipv4/icmp.c
+++ src/ehip-ipv4/icmp.c
@@ -8,7 +8,8 @@
  *
  */
 
-
+#include <eh_debug.h>
+#include <ehip_chksum.h>
 #include <ehip-ipv4/icmp.h>
 #include <ehip-ipv4/ip_message.h>
 
@@ -16,11 +17,35 @@ void icmp_input(struct ip_message *ip_msg){
     int ret;
     struct icmp_hdr *icmp_hdr;
     struct icmp_hdr icmp_hdr_tmp;
+    uint16_t        checksum = 0;
 
     ret = ip_message_rx_read(ip_msg, (uint8_t**)&icmp_hdr, sizeof(struct icmp_hdr), (uint8_t*)&icmp_hdr_tmp);
     if(ret != sizeof(struct icmp_hdr)){
         goto drop;
     }
+    
+    checksum = ehip_inet_chksum_accumulated(checksum, (uint8_t*)icmp_hdr, sizeof(struct icmp_hdr));
+    if(ip_message_flag_is_fragment(ip_msg)){
+        ehip_buffer_t *pos_buffer;
+        int tmp_i, tmp_sort_i;
+        uint16_t single_chksum_len;
+        /* 分片数据校验 */
+        ip_message_rx_fragment_for_each(pos_buffer, tmp_i, tmp_sort_i, ip_msg){
+            single_chksum_len = ehip_buffer_get_payload_size(pos_buffer);
+            checksum = ehip_inet_chksum_accumulated(checksum, 
+                ehip_buffer_get_payload_ptr(pos_buffer), single_chksum_len);
+        }
+
+    }else{
+        checksum = ehip_inet_chksum_accumulated(checksum, 
+            ehip_buffer_get_payload_ptr(ip_msg->buffer), ehip_buffer_get_payload_size(ip_msg->buffer));
+    }
+
+    if(checksum != 0){
+        eh_mwarnfl(ICMP_INPUT, "icmp checksum error %#hx", icmp_hdr->checksum);
+        goto drop;
+    }
+
     switch(icmp_hdr->type){
         case ICMP_TYPE_ECHO_REPLY:
         case ICMP_TYPE_ECHO:{
